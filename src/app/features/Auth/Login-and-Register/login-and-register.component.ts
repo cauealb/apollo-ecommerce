e8877b22bb4bf67c23f7f25bd547fcbdc9a945f2
Cauê Alves <cauealvesb4@gmail.com>
2025-08-02 11:54:42 -0300

    feat: Melhorando validações do formulário

--- src/app/features/Auth/Login-and-Register/login-and-register.component.ts
+++ src/app/features/Auth/Login-and-Register/login-and-register.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-and-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login-and-register.component.html',
   styleUrl: './login-and-register.component.css',
 })
-export class LoginAndRegisterComponent {
-  formLogin = new FormGroup({
-    login: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-  });
+export class LoginAndRegisterComponent implements OnInit {
+  formLogin!: FormGroup
+
+  resetForm() {
+    this.formLogin = new FormGroup({
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+    this.resetForm();
+  }
+
+  enterLogin() {
+    if(this.formLogin.invalid) return;
+    console.log(this.formLogin.value)
+    this.resetForm();
+  }
 }
