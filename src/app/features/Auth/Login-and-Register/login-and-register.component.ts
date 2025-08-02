a10e24cd0ddcea592950a150beefc549e733bd54
Cauê Alves <cauealvesb4@gmail.com>
2025-08-02 11:19:56 -0300

    feat: Vinculando ao formulário no HTML

--- src/app/features/Auth/Login-and-Register/login-and-register.component.ts
+++ src/app/features/Auth/Login-and-Register/login-and-register.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-and-register',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login-and-register.component.html',
-  styleUrl: './login-and-register.component.css'
+  styleUrl: './login-and-register.component.css',
 })
 export class LoginAndRegisterComponent {
   formLogin = new FormGroup({
     login: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
-  })
+    password: new FormControl('', [Validators.required]),
+  });
 }
