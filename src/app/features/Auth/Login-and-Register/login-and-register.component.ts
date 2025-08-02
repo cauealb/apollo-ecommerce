3a8528d8f43a3a5fccbe200a278df645c8addc12
Cauê Alves <cauealvesb4@gmail.com>
2025-08-02 11:14:38 -0300

    feat: Criando validações para o formulário

--- src/app/features/Auth/Login-and-Register/login-and-register.component.ts
+++ src/app/features/Auth/Login-and-Register/login-and-register.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-and-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-and-register.component.html',
   styleUrl: './login-and-register.component.css'
 })
 export class LoginAndRegisterComponent {
-
+  formLogin = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
 }
