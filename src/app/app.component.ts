fdff9c9286ecb38ae10ff5125c4f7cb2e46e0689
Cauê Alves <cauealvesb4@gmail.com>
2025-08-02 10:55:37 -0300

    style: Finalizando estilos, cores e fontes do login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginAndRegisterComponent } from './features/Auth/Login-and-Register/login-and-register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginAndRegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
