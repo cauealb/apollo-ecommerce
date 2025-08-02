bd20662c44134e1ecbcb9d8dfa1029522ae03db4
Cauê Alves <cauealvesb4@gmail.com>
2025-08-02 09:22:48 -0300

    style: Iniciando estilização do form de login

---
+++ src/app/features/Auth/Login-and-Register/login-and-register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-and-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-and-register.component.html',
+  styleUrl: './login-and-register.component.css'
+})
+export class LoginAndRegisterComponent {
+
+}
