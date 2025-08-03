27066d0b1c5985283f2759004202892d71efa5dc
Cauê Alves <cauealvesb4@gmail.com>
2025-08-03 18:10:26 -0300

    feat> Iniciando a lógica do componente

--- src/app/features/Auth/forgot-password/forgot-password/forgot-password.component.ts
+++ src/app/features/Auth/forgot-password/forgot-password/forgot-password.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit {
   
-}
+  formPassword!: FormGroup
+
+  ngOnInit() {
+    console.log('oi')
+  }
+
+  resetGroup() {
+
+  }
+
+  saveNewPassword() {
+    
+  }
+} 
