7fafe52afcb20a12721d363a139c65975e74a178
Cauê Alves <cauealvesb4@gmail.com>
2025-08-03 17:35:32 -0300

    feat: Implementando rotas

--- src/app/features/Auth/forgot-password/forgot-password/forgot-password.component.ts
+++ src/app/features/Auth/forgot-password/forgot-password/forgot-password.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
