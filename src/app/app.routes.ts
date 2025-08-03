008e19e3516d4902fceaaca76f75e999c0596765
Cauê Alves <cauealvesb4@gmail.com>
2025-08-03 16:57:23 -0300

    feat: Adicionado a primeira rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginAndRegisterComponent } from './features/Auth/Login-and-Register/login-and-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginAndRegisterComponent
+    },
+]
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations(), provideToastr()],
+  providers: [provideAnimations(), provideToastr(), provideRouter(routes)],
 }).catch((err) => console.error(err));
