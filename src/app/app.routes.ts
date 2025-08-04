989529d20c74714559dc9a99797d51a03e880ee0
Cauê Alves <cauealvesb4@gmail.com>
2025-08-03 21:28:47 -0300

    chore: Mudando local das pastas dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginAndRegisterComponent } from './features/Auth/Login-and-Register/login-and-register.component';
-import { ForgotPasswordComponent } from './features/Auth/forgot-password/forgot-password/forgot-password.component';
+import { ForgotPasswordComponent } from './features/Auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
